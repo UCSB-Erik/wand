107c040994659ba226d6947e9c6d8b47f9872102
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2015-08-14 21:15:00 -0400

    plotting

--- kmerhistograms.R
+++ kmerhistograms.R
@@ -3,9 +3,11 @@ histb <- read.table("bbhist.txt")
 histc <- read.table("cchist.txt")
 histd <- read.table("ddhist.txt")
 
+pdf("kmer10hists.pdf")
 par(mfrow=c(2,2))
 hist(hista[,1],breaks=100,ylim=c(0,20))
 hist(histb[,1],breaks=100,ylim=c(0,20))
 hist(histc[,1],breaks=100,ylim=c(0,20))
 hist(histd[,1],breaks=100,ylim=c(0,40))
 
+dev.off()
